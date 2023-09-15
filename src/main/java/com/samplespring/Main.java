848ad98c75e7573a6874e99b25672efb2010f7ab
noobmaster69jr <srashwin22@gmail.com>
2023-09-15 16:21:53 +0530

    @SpringBootApplication

--- src/main/java/com/samplespring/Main.java
+++ src/main/java/com/samplespring/Main.java
@@ -1,11 +1,17 @@
 package com.samplespring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = "com.samplespring")
+@EnableAutoConfiguration
 @RestController
 public class Main {
 
