ba81ad71ca89f99e0951058256f2d16fc73a31ef
noobmaster69jr <srashwin22@gmail.com>
2023-09-15 18:21:12 +0530

    change

--- src/main/java/com/samplespring/Main.java
+++ src/main/java/com/samplespring/Main.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication // combination of configuration, ComponentScan, EnableAutoConfiguration
+
 @RestController
 public class Main {
 
