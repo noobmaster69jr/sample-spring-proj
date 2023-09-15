6ef2c1370d46e8f85174900c0d90ef8458b10d24
noobmaster69jr <srashwin22@gmail.com>
2023-09-15 17:31:15 +0530

    more json

--- src/main/java/com/samplespring/Main.java
+++ src/main/java/com/samplespring/Main.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -17,10 +19,24 @@ public class Main {
 
     @GetMapping("/greet")
     public GreetResponse greet(){
-        return new GreetResponse("Hello");
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("java", "javascript", "python"),
+                new Person("Alex"));
+        return response;
     }
 
     // @RestController - tells that class is controller and all mapping methods return a json
 
-    record GreetResponse(String greet){}
+    record Person(String name){
+
+    }
+
+    record GreetResponse(String greet,
+                         List<String> favProgrammingLang,
+                         Person person){}
+
+
+
+    //without record we have to create a GreetResponse class and override hashcode,
+    // toString, implement final datamember greet, and public getter ...
 }
